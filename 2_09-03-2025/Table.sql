d14224b1bd6a26be543cc44fcbe40e618070870e
darshitkansara94 <48837243+darshitkansara94@users.noreply.github.com>
2025-03-09 12:14:13 +0530

    Date 09 March 2025
    Datatype and Table

---
+++ 2_09-03-2025/Table.sql
@@ -0,0 +1,25 @@
+use Amazon_Database
+
+Table :
+	Table contain rows and columns.
+	Every table may contain some data.
+
+	-- Table creation standards
+		When we create table, tanle name start with 'tbl_'
+			Ex : OrderMaster
+			 Table Name should be 'tbl_OrderMaster'
+		Space is not alloed in table name. instead of space we can use underscore (_).
+		Special char are not allowed at the starting.
+
+		-- Syntax
+			Create table tbl_Name
+			(
+				Column_name datatype,
+				Column_name datatype,
+				.
+				.
+				.
+				Column_name datatype
+			)	
+		
+					
